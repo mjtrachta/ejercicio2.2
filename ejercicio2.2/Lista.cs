c634d6793415bd6c61b7d3c46f79f3d43dae772a
mjtrachta <61702195+mjtrachta@users.noreply.github.com>
2022-10-26 13:04:12 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio2.2/Lista.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+
+namespace ejercicio2._2
+{
+    class JavierList
+    {
+        
+        public  void Menu()
+        {
+            Console.WriteLine("+------------------------------------------------------------------+");
+            Console.WriteLine("|   1. Agregar un elemento al final de la lista                    |");
+            Console.WriteLine("|   2. Agregar un elemento en alguna posición de la lista          |");
+            Console.WriteLine("|   3. Eliminar un elemento                                        |");
+            Console.WriteLine("|   4. Buscar un elemento en la lista(devolviendo su posición)     |");
+            Console.WriteLine("|   5. Buscar un elemento en la posición de la lista indicada     |");
+            Console.WriteLine("|   6. Ordenar los elementos de la lista de menor a mayor          |");
+            Console.WriteLine("|   7. Salir                                                       |");
+            Console.WriteLine("+------------------------------------------------------------------+");
+
+            Console.WriteLine("|                     Elige una de las opciones:                   |");
+
+            Console.WriteLine("+------------------------------------------------------------------+");
+
+        }
+        public void OpcionNro(int n)
+        {
+            Console.WriteLine("+-----------------------------------------------------------+");
+            Console.WriteLine("|                   Has elegido la opción {0}                 |", n);
+            Console.WriteLine("+-----------------------------------------------------------+");
+        }
+
+        public void Print(int newNroElementos, int[] newJavierList)
+        {
+            for (int i = 0; i < newNroElementos; i++)
+            {
+                Console.WriteLine("|                            {0}                            |", newJavierList[i]);
+                Console.WriteLine("+-----------------------------------------------------------+");
+            }
+        }
+
+        public void Caso(int nroElementos, int posicion, int[] newJavierList, int[] javierList, int numeroAlFinal )
+        {
+
+            for (int i = 0; i < nroElementos + 1; i++)
+            {
+                if (i < posicion - 1)
+                {
+                    newJavierList[i] = javierList[i];
+                }
+                else if (i == posicion - 1)
+                {
+                    newJavierList[i] = numeroAlFinal;
+                }
+                else
+                {
+                    newJavierList[i] = javierList[i - 1];
+                }
+            }
+        }
+
+        public void Borrar(int posicion, int[] newJavierList, int[] javierList)
+        {
+            
+
+            for (int i = 0; i < newJavierList.Length ; i++)
+            {
+                if (i < posicion - 1)
+                {
+                    newJavierList[i] = javierList[i];
+                }
+                else
+                {
+                    newJavierList[i] = javierList[i+1];
+                }
+            }
+        }
+
+        public void PrintL(string n,string l)
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("|        Por favor ingresa {0} que desea {1}      ", n,l);
+        }
+
+        public void PrintLargo(int nroElementos)
+        {
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("|        Recuerde que la lista tiene {0} elementos         ", nroElementos);
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+
+        public void PrintError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("|  ERROR !! Elige una opcion entre 1 y 6  |");
+            Console.WriteLine("-------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void PrintError2()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("|     ERROR Ingrese un número valido.     |");
+            Console.WriteLine("-------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+
+    }
+}
---
+++ ejercicio2.2/Program.cs
@@ -0,0 +1,151 @@
+﻿using ejercicio2._2;
+using System;
+
+
+namespace ejercicio2
+{
+    public class Program
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            JavierList javier = new JavierList();
+            
+
+            int n = 10;
+            
+            int[] javierList = new int[n];
+
+            //cargo el array con 10 elementos
+            for (int i = 0; i < n; i++)
+            {
+                javierList[i] = i + 1;
+            }
+
+            bool salir = false;
+
+
+            while (!salir)
+            {
+                int nroElementos = javierList.Length;
+
+                int[] newJavierList = new int[nroElementos + 1];
+
+                int newNroElementos = newJavierList.Length;
+
+                javier.Menu();
+
+
+                try
+                {
+                    int opcion = Int32.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+
+                            javier.OpcionNro(1);
+                            javier.PrintL("el número", "agregar");
+                            int numeroAlFinal = Int32.Parse(Console.ReadLine());
+                            int posicion = nroElementos + 1;
+
+                            javier.Caso(nroElementos, posicion, newJavierList, javierList, numeroAlFinal);
+
+                            javierList = newJavierList;
+
+                            javier.Print(newNroElementos, newJavierList);
+                        
+                            break;
+
+                        case 2:
+                            javier.OpcionNro(2);
+                            javier.PrintL("el número", "agregar");
+                            int numeroEnPosicion = Int32.Parse(Console.ReadLine());
+                            javier.PrintLargo(nroElementos);
+                            javier.PrintL("la posición en la", "agregar");
+                            int posicion2 = Int32.Parse(Console.ReadLine());
+
+                            javier.Caso(nroElementos, posicion2, newJavierList, javierList, numeroEnPosicion);
+
+                            javierList = newJavierList;
+
+                            javier.Print(newNroElementos, newJavierList);
+
+                            break;
+
+
+                        case 3:
+
+                            javier.OpcionNro(2);
+                            javier.PrintLargo(nroElementos);
+                            javier.PrintL("el número", "borrar");
+                            int posicion3 = (Int32.Parse(Console.ReadLine()));
+                            javier.OpcionNro(3);
+
+                            newJavierList = new int[javierList.Length -1];
+
+                            Console.WriteLine("old" + javierList.Length + "new" + newJavierList.Length);
+                            
+                            //javierList = javierList.Where(e => e != posicion3).ToArray();
+
+                            javier.Borrar(posicion3,  newJavierList,  javierList);
+
+
+                            javierList = newJavierList;
+
+                            javier.Print(newJavierList.Length, newJavierList);
+
+                        break;
+                        case 4:
+                            javier.OpcionNro(4);
+                            javier.PrintL("el número", "buscar");
+                            int numero4 = Int32.Parse(Console.ReadLine());
+                            int ubicacion4 = (Array.IndexOf(javierList, numero4)) + 1;
+                            if (ubicacion4 == 0)
+                            {
+                                Console.WriteLine("El número {0} no se encontró", numero4);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El número {0} está en la posición: {1}", numero4, ubicacion4);
+                            }
+
+                            break;
+                        case 5:
+                            javier.OpcionNro(5);
+
+                            javier.PrintL("la posición", "buscar");
+                            int ubicacion5 = Int32.Parse(Console.ReadLine());
+                            int numero5 = javierList[ubicacion5-1];
+                            Console.WriteLine("En la posición {0} esta el número: {1}", ubicacion5, numero5);
+
+                            break;
+                        case 6:
+                            javier.OpcionNro(5);
+
+                            Array.Sort(javierList);
+
+                            javier.Print(nroElementos, javierList);
+
+                            break;
+                        case 7:
+                            salir = true;
+                            break;
+
+                        default:
+                            javier.PrintError();
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    javier.PrintError2();
+                }
+
+            }
+        }
+
+    }
+}
+
